5079a5c01ac5cfe9244c1d50c16c17b64b33f72a
Salah <Salah>
2024-09-04 14:58:29 +0100

    caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -2,60 +2,52 @@
 """ 100-lfu_cache.py """
 
 from base_caching import BaseCaching
-from collections import defaultdict
+from collections import OrderedDict
+import operator
 
 
 class LFUCache(BaseCaching):
-    """ LFUCache Class
+    """LFUCache Class
     """
-
     def __init__(self):
         """ __init__
         """
         super().__init__()
-        self.frequency = defaultdict(int)
-        self.usage_order = []
+        self.frequency = OrderedDict()
+        self.sorted_items = None
 
     def put(self, key, item):
         """ put function
         """
-        if key is not None and item is not None:
+        if (len(self.cache_data) == self.MAX_ITEMS and
+                key not in self.frequency):
+            self.sorted_items = sorted(
+                self.frequency.items(), key=operator.itemgetter(1)
+            )
+            discard = self.sorted_items.pop(0)[0]
+            del self.frequency[discard]
+            del self.cache_data[discard]
+            print("DISCARD: {}".format(discard))
+
+        if key and item:
+            value = 0
             if key in self.cache_data:
-                self.usage_order.remove(key)
+                value = self.frequency[key]
+                del self.frequency[key]
+
+            self.frequency[key] = value + 1
             self.cache_data[key] = item
-            self.frequency[key] += 1
-            self.usage_order.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                self.evict()
 
     def get(self, key):
         """ get function
         """
-        if key in self.cache_data:
-            self.frequency[key] += 1
-            self.usage_order.remove(key)
-            self.usage_order.append(key)
-            return self.cache_data[key]
-        return None
+        if not key or key not in self.cache_data:
+            return None
 
-    def evict(self):
-        """ evict function
-        """
-        min_freq = min(self.frequency.values())
-        lfu_keys = [k for k, v in self.frequency.items() if v == min_freq]
-        if len(lfu_keys) > 1:
-            lru_key = None
-            for key in self.usage_order:
-                if key in lfu_keys:
-                    lru_key = key
-                    break
-            self.usage_order.remove(lru_key)
-            del self.cache_data[lru_key]
-            del self.frequency[lru_key]
-            print(f"DISCARD: {lru_key}")
-        else:
-            lfu_key = lfu_keys[0]
-            self.usage_order.remove(lfu_key)
-            del self.cache_data[lfu_key]
-            del self.frequency[lfu_key]
-            print(f"DISCARD: {lfu_key}")
+        value = 0
+        if key in self.frequency.keys():
+            value = self.frequency[key]
+            del self.frequency[key]
+
+        self.frequency[key] = value + 1
+        return self.cache_data[key]
